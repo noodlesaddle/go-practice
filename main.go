31f625f9f10dcadbbc9c64c9b7e919aacca65eea
noodlesaddle <tserenbal@miraic.com>
2023-11-14 16:15:57 +0900

    variable decleration practice

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello there")
+	cards := []string{"Ace of diamonds", newCard()}
+	cards = append(cards, "Queen of Diamond")
+
+	for i, card := range cards {
+
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "five of Diamonds"
 }
