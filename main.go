d52d881ed82e69d62a5b2c4a70dfbd0d50652670
noodlesaddle@gmail.com <noodlesaddle@gmail.com>
2024-01-02 22:21:24 +0900

    changes

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
-func main() {
-	cards := newDeck()
-	cards.shuffleDeck()
-	cards.print()
-	// hand, remainingCards := deal(cards, 5)
-	// hand.print()
-	// remainingCards.print()
-	// cards.saveToFile("my_cards")
+import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email: "test@test.com", zipCode: 84000,
+		},
+	}
+
+	//@definition: &variable = give the memory address is pointing at
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	//@definition: *pointer= take the address and give me the value in the address
+	(*pointerToPerson).firstName = newFirstName
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
